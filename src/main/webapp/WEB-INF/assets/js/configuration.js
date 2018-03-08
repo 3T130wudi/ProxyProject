c7f4bff96e5ff55aa4041315d3c8b4d56dd087b1
2530686142 <2530686142@qq.com>
2018-03-08 14:46:13 +0800

    删除和修改

--- src/main/webapp/WEB-INF/assets/js/configuration.js
+++ src/main/webapp/WEB-INF/assets/js/configuration.js
@@ -19,22 +19,53 @@
 //     });
 // });
 
+
+/*
 function deleteConfig(id) {
+
+    var Name = $("#configId").val();
+    var flag = confirm("确定要删除" + Name + "的信息吗?");
+    if(flag) {
+        $.ajax({
+            type: "POST",
+            url: "/deletefinance",
+            data: {id: id},
+            dataType: "json",
+            success: function (date) {
+                if (date) {
+
+                    window.location.reload();
+
+                } else {
+                    alert('删除失败')
+                }
+            },
+            error: function () {
+                alert("对不起内部服务器错误");
+            }
+        });
+    }
+
+}
+*/
+function selectfanace(id){
+
     $.ajax({
-        type:"POST",
-        url:"/deletefinance",
-        data:{id:id},
-        dataType:"json",
-        success:function (date) {
-            if(date){
-                alert('删除成功');
-                location.reload();
-            }else{
-                alert('删除失败')
+        type: "POST",
+        url: "/selectfnance",
+        data: {fid: id},
+        dataType: "json",
+        success: function (date) {
+            if (date!=null) {
+            $("#financetype").val(date.finance_type);
+            $("#financeenable").val(date.finance_enable);
+            } else {
+
             }
         },
-        error:function () {
+        error: function () {
             alert("对不起内部服务器错误");
         }
     });
+
 }
