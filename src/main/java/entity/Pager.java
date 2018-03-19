c63657b3159d0cd02cf95b8a57a7f04908aa7a3b
472462788 <472462788@qq.com>
2018-03-19 10:30:21 +0800

    分页

---
+++ src/main/java/entity/Pager.java
@@ -0,0 +1,51 @@
+package entity;
+
+import java.util.List;
+
+public class Pager<T> {
+    private int pageNo; //当前页
+    private int pageSize;//每页多少行数据
+    private int totalRows;//总行数
+    private int totalPage;//总页数
+    private List<T> datas; //当前页数据
+
+    public int getPageNo() {
+        return pageNo;
+    }
+
+    public void setPageNo(int pageNo) {
+        this.pageNo = pageNo;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public int getTotalRows() {
+        return totalRows;
+    }
+
+    public void setTotalRows(int totalRows) {
+        this.totalRows = totalRows;
+    }
+
+    public int getTotalPage() {
+        return totalPage;
+    }
+
+    public void setTotalPage(int totalPage) {
+        this.totalPage = totalPage;
+    }
+
+    public List<T> getDatas() {
+        return datas;
+    }
+
+    public void setDatas(List<T> datas) {
+        this.datas = datas;
+    }
+}
